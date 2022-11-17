39e88a89f535551a4101b068f2ffe438d551f6b6
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2022-11-18 01:35:35 +0900

    TopPage.jsをtsx化

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -4,12 +4,28 @@ import Title from '../components/Title';
 import Selector from '../components/Selector';
 import Results from '../components/Results';
 
+type TopPageType = {
+    countriesJson: {
+        Country: string,
+        Slug: string,
+    }[],
+    setCountry: React.Dispatch<React.SetStateAction<string>>,
+    countryData: {
+        date: string,
+        newConfirmed: number,
+        totalConfirmed: number,
+        newRecovered: number,
+        totalRecovered: number,
+    },
+    loading: boolean,
+}
+
 const TopPage = ({
     countriesJson,
     setCountry,
     countryData,
     loading,
-}) => {
+}: TopPageType) => {
     return (
         <div className='top-page-container'>
             <div>
